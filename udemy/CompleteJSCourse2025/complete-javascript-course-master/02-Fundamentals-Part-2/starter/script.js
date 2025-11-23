95cc609caaae4ffce7d096d2c3e099691e808f17
deusmortis2 <deusmortisii@gmail.com>
2025-11-23 17:00:03 -0600

    Added some udemy course things, and some new stuff to loops in FCC

--- udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,3 +1,4 @@
+/*
 // 33. Strict Mode
 "use strict"; // this has to be the first statement in a script
 
@@ -9,3 +10,32 @@ if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = "Audio"; reserved keyword
 // const private = 534; private is another future reserved keyword
+
+
+// 34. Functions
+function logger() {
+  console.log(`My name is Jonas`);
+}
+
+//calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+*/
+
+//35. Function Declarations vs Expressions

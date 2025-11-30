c1b15edf37d6bdbf4b4bd3da02a3849440f9c42c
deusmortis2 <deusmortisii@gmail.com>
2025-11-30 16:57:08 -0600

    Completed section 2 content for udemy course includiing challenges

--- udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -309,7 +309,7 @@ console.log(jonas.getSummary());
 for (let rep = 1; rep <= 30; rep++) {
   console.log(`Lifting weights repetition ${rep} 🏋️`);
 }
-*/
+
 // 48. Looping Arrays, Breaking and Continuing
 const jonas = ["Jonas", "Schmedtmann", 2037 - 1991, "teacher", ["Michael", "Peter", "Steven"], true];
 
@@ -318,9 +318,81 @@ const jonas = ["Jonas", "Schmedtmann", 2037 - 1991, "teacher", ["Michael", "Pete
 //console.log(jonas[4]
 //jonas[5] does not exist)
 const types = [];
-for (let i = 0; i < jonas.length; i++) {
-  console.log(jonas[i], typeof jonas[i]);
-  types[i] = typeof jonas[i];
-}
+// for (let i = 0; i < jonas.length; i++) {
+//   //reading from jonas array
+//   console.log(jonas[i], typeof jonas[i]);
+
+//   //filling types array
+//   // types[i] = typeof jonas[i];
+//   types.push(typeof jonas[i]);
+// }
 
 console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+//continue and break
+
+console.log("--- ONLY STRINGS ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue;
+  console.log(jonas[i], typeof jonas[i]);
+  types.push(typeof jonas[i]);
+}
+
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") break;
+  console.log(jonas[i], typeof jonas[i]);
+  types.push(typeof jonas[i]);
+}
+
+
+
+// 49. Looping Backwards and Loops in Loops
+
+const jonas = ["Jonas", "Schmedtmann", 2037 - 1991, "teacher", ["Michael", "Peter", "Steven"]];
+
+// 0, 1, ..., 4
+// 4,3, ..., 0
+
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep} 🏋️`);
+  }
+}
+
+
+// 50. The while loop
+
+for (let rep = 1; rep <= 10; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️`);
+}
+
+let rep = 1;
+while (rep <= 10) {
+  console.log(`WHILE: Lifting weights repetition ${rep} 🏋️`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
+}
+*/
---
+++ udemy/CompleteJSCourse2025/complete-javascript-course-master/Challenges/s2Challenge4.js
@@ -0,0 +1,27 @@
+"use strict";
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+for (let i = 0; i < bills.length; i++) {
+  tips[i] = calcTip(bills[i]);
+  totals[i] = bills[i] + tips[i];
+}
+
+function calcAverage(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+}
+console.log(bills);
+console.log(tips);
+console.log(totals);
+console.log(calcAverage(totals));

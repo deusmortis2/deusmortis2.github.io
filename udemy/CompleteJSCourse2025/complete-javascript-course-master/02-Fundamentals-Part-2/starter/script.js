5d796d675604555c7995b765b2ddd668503655a3
deusmortis2 <deusmortisii@gmail.com>
2025-11-29 23:42:34 -0600

    More content for udemy section and notes

--- udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ udemy/CompleteJSCourse2025/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -291,4 +291,23 @@ console.log(jonas.getSummary());
 
 //Challenge
 //"Jonas is a 46-year old teacher, and he has a driver's license. "
+
+
+// 47. Iteration: The for loop
+// console.log("Lifting weights repetition 1 🏋️");
+// console.log("Lifting weights repetition 2 🏋️");
+// console.log("Lifting weights repetition 3 🏋️");
+// console.log("Lifting weights repetition 4 🏋️");
+// console.log("Lifting weights repetition 5 🏋️");
+// console.log("Lifting weights repetition 6 🏋️");
+// console.log("Lifting weights repetition 7 🏋️");
+// console.log("Lifting weights repetition 8 🏋️");
+// console.log("Lifting weights repetition 9 🏋️");
+// console.log("Lifting weights repetition 10 🏋️");
+
+//for loop keeps running while condition is TRUE
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights repetition ${rep} 🏋️`);
+}
 */
+// 48. Looping Arrays, Breaking and Continuing

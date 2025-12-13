649e74c79ae3c7f7abcca250d4801c80e361cf4f
deusmortis2 <deusmortisii@gmail.com>
2025-12-12 22:52:42 -0600

    Synced some changes into the environment from other desktop. Hoping to not create merge problems while still keeping up with work in different spaces.

--- udemy/CompleteJSCourse2025/complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ udemy/CompleteJSCourse2025/complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -16,8 +16,6 @@ console.log(calcAge(1991));
 // Problem:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
-
 // 1. Understanding the problem.
 // - What is temperature amplitude? Answer: difference between highest and lowest temp.
 // How to computer the maximum and minimum temperatures.
@@ -29,4 +27,31 @@ const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
 // Find min value in array
 // Subtract min from max and return amplitude
 
-const calcTempAmplitude = function (temps) {};
+const temperatures = [3, -2, -6, -1, "error", 9, 13, 17, 15, 14, 9, 5];
+const calcTempAmplitudeNew = function (t1, t2) {
+  const temps = t1.concat(t2);
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== "number") continue;
+    if (curTemp > max) max = temps[i];
+    if (curTemp < min) min = temps[i];
+  }
+  console.log(max);
+  console.log(min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+console.log(amplitudeNew);
+
+// Problem 2:
+// Function should now receive 2 arrays of temps
+
+// 1. Understanding the problem
+// With 2 arrays, should we implement functionality twice? No! Just merge two arrays
+
+// 2. Breaking it up into sub-problems
+// - Merge 2 arrays
+// - Send merged array to the amplitude function

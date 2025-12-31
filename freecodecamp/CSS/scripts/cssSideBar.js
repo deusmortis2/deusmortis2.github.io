0126094dedd9a948e065413995528107ab594022
deusmortis2 <deusmortisii@gmail.com>
2025-12-30 19:09:49 -0600

    forgot to link script

--- freecodecamp/CSS/scripts/cssSideBar.js
+++ freecodecamp/CSS/scripts/cssSideBar.js
@@ -4,7 +4,7 @@ const toggleButton = document.getElementById('toggle-btn');
 const sidebar = document.getElementById('sidebar');
 
 function toggleSideBar() {
-    sidebar.classList.toggle('close');
+    sidebar.classList.toggle("close");
     toggleButton.classList.toggle('rotate');
 
     closeAllSubMenus();

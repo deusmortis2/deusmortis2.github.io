6a9cc4c80c5e171f7678d6113a2cd4be35cb3948
deusmortis2 <deusmortisii@gmail.com>
2026-04-27 11:44:47 -0500

    updated calculator project

---
+++ Personal-Projects/Calculator/script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const output = document.getElementById("output");
+const dpad = document.getElementById("dpadholder");
+const dpadleft = document.getElementById("dleft");
+const dpadright = document.getElementById("dright");
+const numleft = document.getElementById("nleft");
+const nummiddle = document.getElementById("nmiddle");
+const numright = document.getElementById("nright");

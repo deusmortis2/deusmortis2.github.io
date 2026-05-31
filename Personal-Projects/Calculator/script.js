46255ba5e5ea814709b6191524ede689d39abf2e
deusmortis2 <deusmortisii@gmail.com>
2026-05-30 22:46:02 -0500

    notes updates etce

--- Personal-Projects/Calculator/script.js
+++ Personal-Projects/Calculator/script.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const output = document.getElementById("output");
-const dpad = document.getElementById("dpadholder");
-const dpadleft = document.getElementById("dleft");
-const dpadright = document.getElementById("dright");
-const numleft = document.getElementById("nleft");
-const nummiddle = document.getElementById("nmiddle");
-const numright = document.getElementById("nright");
+const calculator = document.getElementById("Full");
+
+calculator.addEventListener("click", (event) => {
+    const button = event.target.closest("button");
+    if (!button) return;
+    output.textContent += button.id + " ";
+     
+    });
